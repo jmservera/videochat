1d03818a7cbea0aecb49025cc289e70fccdf2e92
Juan Manuel Servera Bondroit <juanserv@microsoft.com>
2020-06-01 01:06:32 +0200

    fix openssl

--- turnserver/scripts/installturn.sh
+++ turnserver/scripts/installturn.sh
@@ -9,7 +9,8 @@ pkey=/etc/ssl/turn_server_pkey.pem
  
 no-stdout-log"  | tee /etc/turnserver.conf
 
-openssl req -x509 -newkey rsa:4096 -keyout /etc/ssl/turn_server_pkey.pem -out /etc/ssl/turn_server_cert.pem -days 365
+#generate self signed certificates, use real ones for a production server
+openssl req -x509 -newkey rsa:4096 -passout pass:falsepass -keyout /etc/ssl/turn_server_pkey.pem -out /etc/ssl/turn_server_cert.pem -days 365 -subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
 
 turnadmin -a -u $1 -p $2 -r $3
 
